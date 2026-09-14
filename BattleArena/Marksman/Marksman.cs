62632da024d09ba9e115b0a6a098b3ffa913fd1c
kennnn10 <kennethyagyagan1@gmail.com>
2026-09-14 14:12:33 +0800

    Refactor Battle Logic add Fighter, Marksman, and Tank and Classes

---
+++ BattleArena/Marksman/Marksman.cs
@@ -0,0 +1,46 @@
+﻿using BattleArena.Warriors;
+using System;
+using System.Threading;
+using System.Xml.Linq;
+
+namespace BattleArena.Warriors
+{
+    public class Marksman : Warrior
+    {
+        public int ArrowDamage { get; private set; }
+        public Marksman(string name, int health, int attackPower, int arrowDamage)
+            : base(name, health, attackPower)
+        {
+            ArrowDamage = arrowDamage;
+            attackPower += arrowDamage;
+        }
+
+        public Marksman(string name, int health, int attackPower)
+            : base(name, health, attackPower)
+        {
+
+        }
+
+        public override void Attack(Warrior target)
+        {
+            var totalDamage = target.AttackPower + ArrowDamage;
+            TakeDamage(totalDamage);
+            Console.WriteLine($"\t-> {Name}: Ano bai kaya pa? {target.Name}!");
+            Thread.Sleep(1000);
+
+            Console.WriteLine($"\t-> {target.Name}: Run baby run!");
+            Thread.Sleep(1000);
+
+            if (target.IsAlive)
+                Console.WriteLine($"\t-> {target.Name}: ahhhhh giatay! {target.Name}");
+            Thread.Sleep(1000);
+
+            Console.WriteLine($"\t------ {target.Name} --------");
+            Console.WriteLine($"\t  *Damage Taken: {totalDamage} *");
+            Console.WriteLine($"\t  *Health Remaining: {target.Health} *");
+
+
+
+        }
+    }
+}
--- BattleArena/Program.cs
+++ BattleArena/Program.cs
@@ -1,34 +1,33 @@
-﻿using battleArena.Warrios;
+﻿using BattleArena.Warriors;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace battleArena
+namespace BattleArena
 {
+
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) 
         {
-            Warrior adili = new Warrior("Rene", 100, 30, "Fireball");
-            Warrior kerki = new Warrior("Bosseng", 200, 25, "Lightning Strike");
-            Warrior Jjbsuarez = new Warrior("Jb", 300, 40, "Ice Blast");
-
-
-            adili.DisplayStatus();
-            kerki.DisplayStatus();
-            Jjbsuarez.DisplayStatus();
-
-
             int round = 1;
+            var adili = new Marksman("adili", 100, 30);
+            var kerki = new Mage("kerki", 200, 15);
+            var Jjbsuarez = new Tank("jbsuarez", 150, 30);
+
+            adili.DisplayStats();
+            kerki.DisplayStats();
+            Jjbsuarez.DisplayStats();
+
             while (adili.IsAlive && kerki.IsAlive && Jjbsuarez.IsAlive)
             {
-                Console.WriteLine("----------Round {round} ----------");
+
                 adili.Attack(kerki);
-                kerki.Attack(Jjbsuarez);
+                Console.WriteLine("----------------------------------------------");
                 Jjbsuarez.Attack(adili);
-                Console.WriteLine("--------------------------------");
+                Console.WriteLine("----------------------------------------------");
                 round++;
             }
 
