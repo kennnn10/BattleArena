475b1fb1c601ffcdb09bf6cfecae3b200481ea2b
kennnn10 <kennethyagyagan1@gmail.com>
2026-09-14 13:18:50 +0800

    First Warrior

---
+++ BattleArena/Program.cs
@@ -0,0 +1,42 @@
+﻿using battleArena.Warrios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace battleArena
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Warrior Adili = new Warrior();
+            Adili.Name = "Rene";
+            Adili.Health = 100;
+            Adili.AttackPower = 30;
+
+            Warrior kerki = new Warrior();
+            kerki.Name = "Bosseng";
+            kerki.Health = 200;
+            kerki.AttackPower = 25;
+
+            Warrior jbsuarez = new Warrior();
+            jbsuarez.Name = "Jb";
+            jbsuarez.Health = 300;
+            jbsuarez.AttackPower = 40;
+
+            Console.WriteLine($"{Adili.Name} has  " +
+                $"{Adili.Health} health and {Adili.AttackPower} " + $"attack power.");
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine($"{kerki.Name} has" +
+                $" {kerki.Health} health and {kerki.AttackPower} " + $"attack power.");
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine($"{jbsuarez.Name} has" +
+               $" {jbsuarez.Health} health and {jbsuarez.AttackPower} " + $"attack power.");
+            Console.WriteLine("--------------------------------");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ BattleArena/Warriors/Warrior.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace battleArena.Warrios
+{
+    public class Warrior
+    {
+        public string Name;
+
+        public int Health;
+
+        public int AttackPower;
+    }
+}
